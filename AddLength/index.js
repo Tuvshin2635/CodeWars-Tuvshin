d1918b1d6679b2636ee4afb60075fdd33fe57996
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2023-02-26 19:44:41 +0800

    hw

---
+++ AddLength/index.js
@@ -0,0 +1,8 @@
+let text = "apple ban";
+
+let splits = text.split(" ");
+
+console.log(splits[0], splits[0].length, splits[1], splits[1].length);
+// console.log(splits1, splits[0].length);
+
+// for guigene,
---
+++ Price of Mangoes/index.js
@@ -0,0 +1,10 @@
+let price = 6;
+let quantity = 3;
+
+function mango(quantity, price) {
+  if (quantity % 3 === 0) return console.log(quantity);
+  else {
+    console.log(price);
+  }
+  return mango;
+}
